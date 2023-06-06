42de9de8fffa9394cf1c53d389c2fdd0c0956d8a
Rafael Magaña <raismaav.2510@gmail.com>
2023-06-06 11:11:33 -0600

    Transformaciones terminadas

--- src/Ventana.java
+++ src/Ventana.java
@@ -17,15 +17,138 @@ public class Ventana extends JFrame{
 
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        paint();
+        practica1Obliuca();
+        //practica2Ortogonal();
+        //practica3UnPuntoDeFuga();
+        //practica4DosPuntosDeFuga();
+        //practica5TresPuntosDeFuga();
+        //practica6PuntosDeFuga();
+        //practica7Rotacion();
+        //practica8Traslacion();
+        //practica9Escalacion();
+        //practica10Relleno();
     }
 
     public void paint() {
         double [] projectionPlane = new double[] {10, 10, 50};
         Figures3D figures = new Figures3D();
+        Figures3D pruebas = new Figures3D(figures.getCuboPrueba());
         Graphics3D graphics = new Graphics3D(canvas);
         graphics.setProjectionPlane(projectionPlane);
-        graphics.conicalProjection(figures.getCuboPrueba());
+        graphics.conicalProjection(pruebas.getFigure());
         graphics.conicalProjection(figures.getCuboPrueba2());
+    }
+
+    public void practica1Obliuca() {
+        double [] projectionPlane = new double[] {10, 10, 50};
+        Figures3D figures = new Figures3D(Figures3D.getCuboPrueba());
+        figures.setTranslations(100, -100, 0);
+        Graphics3D graphics = new Graphics3D(canvas);
+        graphics.setProjectionPlane(projectionPlane);
+        graphics.obliqueProjection(figures.getFigure());
+    }
+
+    public void practica2Ortogonal() {
+        double [] projectionPlane = new double[] {10, 10, 50};
+        Figures3D figures = new Figures3D(Figures3D.getCuboPrueba());
+        figures.setTranslations(150, 200, 0);
+        figures.setXAxisRotationAngle(45);
+        figures.setYAxisRotationAngle(45);
+        figures.setZAxisRotationAngle(45);
+        Graphics3D graphics = new Graphics3D(canvas);
+        graphics.setProjectionPlane(projectionPlane);
+        graphics.orthogonalProjection(figures.getFigure());
+    }
+
+    public void practica3UnPuntoDeFuga() {
+        Figures3D figures = new Figures3D(Figures3D.getCuboPrueba());
+        figures.setTranslations(200, 200, 0);
+        Graphics3D graphics = new Graphics3D(canvas);
+        graphics.conicalProjection(figures.getFigure());
+    }
+
+    public void practica4DosPuntosDeFuga() {
+        Figures3D figures = new Figures3D(Figures3D.getCuboPrueba());
+        figures.setTranslations(200, 200, 0);
+        figures.setYAxisRotationAngle(45);
+        Graphics3D graphics = new Graphics3D(canvas);
+        graphics.conicalProjection(figures.getFigure());
+    }
+
+    public void practica5TresPuntosDeFuga() {
+        Figures3D figures = new Figures3D(Figures3D.getCuboPrueba());
+        figures.setTranslations(200, 200, 0);
+        figures.setXAxisRotationAngle(25);
+        figures.setYAxisRotationAngle(45);
+        figures.setZAxisRotationAngle(60);
+        Graphics3D graphics = new Graphics3D(canvas);
+        graphics.conicalProjection(figures.getFigure());
+    }
+
+    public void practica6PuntosDeFuga() {
+        Figures3D figures1 = new Figures3D(Figures3D.getCuboPrueba());
+        figures1.setTranslations(20, 20, 0);
+
+
+        Figures3D figures2 = new Figures3D(Figures3D.getCuboPrueba());
+        figures2.setTranslations(280, 300, 0);
+        figures2.setYAxisRotationAngle(45);
+
+        Figures3D figures3 = new Figures3D(Figures3D.getCuboPrueba());
+        figures3.setTranslations(400, 150, 0);
+        figures3.setXAxisRotationAngle(25);
+        figures3.setYAxisRotationAngle(45);
+        figures3.setZAxisRotationAngle(60);
+
+        Graphics3D graphics = new Graphics3D(canvas);
+        graphics.conicalProjection(figures1.getFigure());
+        graphics.conicalProjection(figures2.getFigure());
+        graphics.conicalProjection(figures3.getFigure());
+    }
+
+    public void practica7Rotacion() {
+        Figures3D figures = new Figures3D(Figures3D.getCuboPrueba());
+        figures.setTranslations(200, 200, 0);
+        for (int i = 0; i < 100; i++) {
+            Graphics3D graphics = new Graphics3D(canvas);
+            graphics.conicalProjection(figures.getFigure());
+            figures.setXAxisRotationAngle(10);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+    public void practica8Traslacion() {
+        Figures3D figures = new Figures3D(Figures3D.getCuboPrueba());
+        for (int i = 0; i < 100; i++) {
+            Graphics3D graphics = new Graphics3D(canvas);
+            graphics.conicalProjection(figures.getFigure());
+            figures.setTranslations(10,10,0);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public void practica9Escalacion() {
+        Figures3D figures = new Figures3D(Figures3D.getCuboPrueba());
+        for (int i = 0; i < 100; i++) {
+            Graphics3D graphics = new Graphics3D(canvas);
+            graphics.conicalProjection(figures.getFigure());
+            figures.setScale(1.01);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public void practica10Relleno() {
+
     }
 }
